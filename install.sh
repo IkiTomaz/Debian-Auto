d9d658594d13b4113537d8674ded9f264473fb55
IkiTomaz <tibiriano.tomaz.1002@gmail.com>
2025-05-21 12:57:10 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 apt update && apt upgrade
 apt install nala neovim vlc gedit gnome-disk-utility sudo timeshift lsd firefox-esr goverlay gparted nautilus gnome-tweaks alacritty
-echo "alias apt='nala'" >> /root/.bashrc
+echo "alias upd='nala update'" >> /root/.bashrc
+echo "alias upg='nala upgrade'" >> /root/.bashrc
+echo "alias upd='nala update'" >> /home/iki/.bashrc
+echo "alias upd='nala upgrade'" >> /home/iki/.bashrc
 echo "alias ls='lsd -1 -a'" >> /root/.bashrc/
 echo "alias ls='lsd -1 -a'" >> /home/iki/.bashrc/
-cp -r /home/iki/Obsidian\ Vault/ /home/iki/Documentos
 cp -r Adwaita /usr/share/fonts/
 cp -r JetBrains /usr/share/fonts/
 apt install flatpak
 apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-cd ..
 cp -r alacritty/ /home/iki/.config
 cp -r .icons/ /home/iki/
 cp -r Imagens-Wallpapers /home/iki/Imagens
